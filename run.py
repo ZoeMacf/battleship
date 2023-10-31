0bd81b85e87454deed6872ce99522b6096e7e95c
ZoeMacf <zoemacfarlane93@gmail.com>
2023-10-31 23:12:17 +0000

    feat: add option to display game board

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 def start_screen():
     """Prints welcome screen with ASCII art and menu page"""
     
@@ -17,10 +19,32 @@ def start_screen():
     print("Welcome to the strategic game of Battleship!\n")
     print("Have you got what it takes to take down your enemy's warships?\n")
     
-    print("What would you like to do?\n")
     print("1. Start Game\n")
     print("2. Rules\n")
     print("3. Exit Game\n")
+    
+    menu_choice = input("Please choose one of the above, using the numbers 1, 2 or 3\n")
+    
+    if menu_choice == '1':
+        display_game_board()
+    else:
+        print("Please choose something else")
+    
+def display_game_board():
+    """
+    Generates a game board for the user
+    and will then prompt the user to make their first choice
+    """
+    game_board = [
+        ['', '', '', '', '',],
+        ['', '', '', '', '',],
+        ['', '', '', '', '',],
+        ['', '', '', '', '',],
+        ['', '', '', '', '',]
+    ]
+    
+    pprint(game_board)
+        
     
 def main():
     """
